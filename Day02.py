7b81b89807e43e6c9c89ba936da7c52d4fe1b466
Metzing Márton <marton.metzing@gmail.com>
2022-12-02 09:50:14 +0100

    02b

--- Day02.py
+++ Day02.py
@@ -1,30 +1,30 @@
 outcomePoints = {
-    'A': {
-        'X': 3,
-        'Y': 6,
-        'Z': 0,
-    },
-    'B': {
-        'X': 0,
-        'Y': 3,
-        'Z': 6,
-    },
-    'C': {
-        'X': 6,
-        'Y': 0,
-        'Z': 3,
-    }
+    'X': 0,
+    'Y': 3,
+    'Z': 6,
 }
 
 guessPoints = {
-    'X': 1,
-    'Y': 2,
-    'Z': 3,
+    'A': {
+        'X': 3,
+        'Y': 1,
+        'Z': 2,
+    },
+    'B': {
+        'X': 1,
+        'Y': 2,
+        'Z': 3,
+    },
+    'C': {
+        'X': 2,
+        'Y': 3,
+        'Z': 1,
+    }
 }
 
 aggr = 0
 
-for [enemy, _space, guess] in (line.strip() for line in open('Day02.txt')):
-    aggr += outcomePoints[enemy][guess] + guessPoints[guess]
+for [enemy, _space, outcome] in (line.strip() for line in open('Day02.txt')):
+    aggr += outcomePoints[outcome] + guessPoints[enemy][outcome]
 
 print(aggr)
