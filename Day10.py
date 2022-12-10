d89a34f7fa18182033fc6cba0ac6557bb5f8fa5a
Metzing Márton <marton.metzing@gmail.com>
2022-12-10 12:43:07 +0100

    10b clean

--- Day10.py
+++ Day10.py
@@ -20,23 +20,15 @@ while len(lines) > 0 or offset != 0:
     if busy == 0 and len(lines) != 0: 
         line = lines.pop(0)
 
-        print(f'Start {cycle}: begin {line}')
-
         if line != 'noop':
             offset = int(re.match(r'addx (?P<offset>[0-9-]+)', line).group('offset'))
             busy = 2
 
-    
-    print(f'During {cycle} cursor ')
-    print(''.join(['#' if near(i, x) else '.' for i in range(1,41)]))
-
     if near(cycle, x):
         line_out += '#'
     else :
-        line_out += '.'
+        line_out += ' '
 
-    print(line_out)
-     
     if cycle in linebreaks:
         output.append(line_out)
         line_out = ''
@@ -47,13 +39,6 @@ while len(lines) > 0 or offset != 0:
         if busy == 0:
             x += offset
             offset = 0
-            print(f'Cursor position:')
-            print(''.join(['#' if near(i, x) else '.' for i in range(0,40)]))
-
-
     cycle += 1
-    print()
 
-print()
-print()
 print('\n'.join(output))
