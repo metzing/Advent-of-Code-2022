fb9a993a582958508a4d1b4cba610a10c6fc6add
Metzing Márton <marton.metzing@gmail.com>
2022-12-10 12:38:11 +0100

    10b readable enough

--- Day10.py
+++ Day10.py
@@ -1,6 +1,6 @@
 import re
 
-measures = [20, 60, 100, 140, 180, 220]
+linebreaks = [40, 80, 120, 160, 200, 240]
 
 lines = [line.strip() for line in open('Day10.txt')]
 
@@ -9,19 +9,37 @@ offset = 0
 cycle = 1
 x = 1
 
-results = []
+line_out = ''
+output = []
+
+def near(cycle, x):
+    return abs(((cycle - 1) % 40) - (x % 40)) <= 1
 
 while len(lines) > 0 or offset != 0:
 
     if busy == 0 and len(lines) != 0: 
         line = lines.pop(0)
 
+        print(f'Start {cycle}: begin {line}')
+
         if line != 'noop':
             offset = int(re.match(r'addx (?P<offset>[0-9-]+)', line).group('offset'))
             busy = 2
 
-    if cycle in measures:
-        results.append((x * cycle))
+    
+    print(f'During {cycle} cursor ')
+    print(''.join(['#' if near(i, x) else '.' for i in range(0,40)]))
+
+    if near(cycle, x):
+        line_out += '#'
+    else :
+        line_out += '.'
+
+    print(line_out)
+     
+    if cycle in linebreaks:
+        output.append(line_out)
+        line_out = ''
 
     if busy > 0:
         busy -= 1
@@ -29,7 +47,13 @@ while len(lines) > 0 or offset != 0:
         if busy == 0:
             x += offset
             offset = 0
+            print(f'Cursor position:')
+            print(''.join(['#' if near(i, x) else '.' for i in range(0,40)]))
+
 
     cycle += 1
+    print()
 
-print(sum(results))
+print()
+print()
+print('\n'.join(output))
